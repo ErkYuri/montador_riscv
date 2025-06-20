03f7e855dcfbf40ed7b7a9c69d0aee06516f2b2c
Larissa Oliveira <larisoliveira25@gmail.com>
2025-06-20 15:34:42 -0300

    montador.c

--- montador.c
+++ montador.c
@@ -1,22 +1,33 @@
-#include <stdio.h>    // Biblioteca para entrada e saída padrão (printf, fopen, etc)
-#include <stdlib.h>   // Biblioteca para funções utilitárias (atoi, malloc, etc)
-#include <string.h>   // Biblioteca para manipulação de strings
+/*
+ * Montador RISC-V Simplificado - Grupo 11
+ * InstruÃ§Ãµes suportadas: lh, sh, add, or, andi, sll, bne
+ * Linguagem: C
+ * Compile com: gcc montador.c -o montador
+ * Rode com: ./montador entrada.asm [-o saida.txt]
+ */
 
-#define MAX_LINE 100  // Tamanho máximo de uma linha lida do arquivo
-#define MAX_BIN 35    // Tamanho máximo de uma instrução em binário (inclui '\0')
+#include <stdio.h>    // Biblioteca padrÃ£o de entrada e saÃ­da
+#include <stdlib.h>   // Biblioteca para uso de atoi, malloc, etc.
+#include <string.h>   // Biblioteca para manipulaÃ§Ã£o de strings
 
+#define MAX_LINE 100  // Tamanho mÃ¡ximo de uma linha do .asm
+#define MAX_BIN 35    // Tamanho mÃ¡ximo da string binÃ¡ria da instruÃ§Ã£o
+
+// Remove o '\n' do final de uma string (linha do arquivo)
 void remove_newline(char *str) {
     str[strcspn(str, "\n")] = 0;
 }
-void reg_to_bin(char *reg, char *bin) {
 
-    int num = atoi(reg + 1);
+// Converte registrador "xN" para binÃ¡rio de 5 bits
+void reg_to_bin(char *reg, char *bin) {
+    int num = atoi(reg + 1); // Ignora o 'x' e pega o nÃºmero
     for (int i = 4; i >= 0; i--) {
         bin[4 - i] = ((num >> i) & 1) + '0';
     }
-    bin[5] = '\0';
+    bin[5] = '\0'; // Finaliza string
 }
 
+// Converte um inteiro para binÃ¡rio com sinal, com quantidade de bits definida
 void imm_to_bin(int num, char *bin, int bits) {
     unsigned val = (unsigned)(num & ((1 << bits) - 1));
     for (int i = bits - 1; i >= 0; i--) {
@@ -25,60 +36,79 @@ void imm_to_bin(int num, char *bin, int bits) {
     bin[bits] = '\0';
 }
 
+// Imprime no terminal e salva no arquivo (se houver)
+void print_and_save(FILE *out, const char *binstr) {
+    printf("%s\n", binstr);               // Sempre imprime no terminal
+    if (out && out != stdout) {
+        fprintf(out, "%s\n", binstr);     // Escreve no arquivo, se necessÃ¡rio
+    }
+}
+
+// Formato R - ADD
 void add_instr(char *rd, char *rs1, char *rs2, FILE *out) {
-    char rd_bin[6], rs1_bin[6], rs2_bin[6];
+    char rd_bin[6], rs1_bin[6], rs2_bin[6], binstr[33];
     reg_to_bin(rd, rd_bin);
     reg_to_bin(rs1, rs1_bin);
     reg_to_bin(rs2, rs2_bin);
-    fprintf(out, "0000000%s%s000%s0110011\n", rs2_bin, rs1_bin, rd_bin);
+    sprintf(binstr, "0000000%s%s000%s0110011", rs2_bin, rs1_bin, rd_bin);
+    print_and_save(out, binstr);
 }
 
+// Formato R - OR
 void or_instr(char *rd, char *rs1, char *rs2, FILE *out) {
-    char rd_bin[6], rs1_bin[6], rs2_bin[6];
+    char rd_bin[6], rs1_bin[6], rs2_bin[6], binstr[33];
     reg_to_bin(rd, rd_bin);
     reg_to_bin(rs1, rs1_bin);
     reg_to_bin(rs2, rs2_bin);
-    fprintf(out, "0000000%s%s110%s0110011\n", rs2_bin, rs1_bin, rd_bin);
+    sprintf(binstr, "0000000%s%s110%s0110011", rs2_bin, rs1_bin, rd_bin);
+    print_and_save(out, binstr);
 }
 
+// Formato I - ANDI
 void andi_instr(char *rd, char *rs1, int imm, FILE *out) {
-    char rd_bin[6], rs1_bin[6], imm_bin[13];
+    char rd_bin[6], rs1_bin[6], imm_bin[13], binstr[33];
     reg_to_bin(rd, rd_bin);
     reg_to_bin(rs1, rs1_bin);
     imm_to_bin(imm, imm_bin, 12);
-    fprintf(out, "%s%s111%s0010011\n", imm_bin, rs1_bin, rd_bin);
+    sprintf(binstr, "%s%s111%s0010011", imm_bin, rs1_bin, rd_bin);
+    print_and_save(out, binstr);
 }
 
+// Formato R - SLL
 void sll_instr(char *rd, char *rs1, char *rs2, FILE *out) {
-    char rd_bin[6], rs1_bin[6], rs2_bin[6];
+    char rd_bin[6], rs1_bin[6], rs2_bin[6], binstr[33];
     reg_to_bin(rd, rd_bin);
     reg_to_bin(rs1, rs1_bin);
     reg_to_bin(rs2, rs2_bin);
-    fprintf(out, "0000000%s%s001%s0110011\n", rs2_bin, rs1_bin, rd_bin);
+    sprintf(binstr, "0000000%s%s001%s0110011", rs2_bin, rs1_bin, rd_bin);
+    print_and_save(out, binstr);
 }
 
+// Formato I - LH
 void lh_instr(char *rd, int imm, char *rs1, FILE *out) {
-    char rd_bin[6], rs1_bin[6], imm_bin[13];
+    char rd_bin[6], rs1_bin[6], imm_bin[13], binstr[33];
     reg_to_bin(rd, rd_bin);
     reg_to_bin(rs1, rs1_bin);
     imm_to_bin(imm, imm_bin, 12);
-    fprintf(out, "%s%s001%s0000011\n", imm_bin, rs1_bin, rd_bin);
+    sprintf(binstr, "%s%s001%s0000011", imm_bin, rs1_bin, rd_bin);
+    print_and_save(out, binstr);
 }
 
+// Formato S - SH
 void sh_instr(char *rs2, int imm, char *rs1, FILE *out) {
-    char rs2_bin[6], rs1_bin[6], imm_bin[13];
-    char imm_hi[8], imm_lo[6];
+    char rs2_bin[6], rs1_bin[6], imm_bin[13], imm_hi[8], imm_lo[6], binstr[33];
     reg_to_bin(rs2, rs2_bin);
     reg_to_bin(rs1, rs1_bin);
     imm_to_bin(imm, imm_bin, 12);
     strncpy(imm_hi, imm_bin, 7); imm_hi[7] = '\0';
     strncpy(imm_lo, imm_bin + 7, 5); imm_lo[5] = '\0';
-    fprintf(out, "%s%s001%s%s0100011\n", imm_hi, rs2_bin, rs1_bin, imm_lo);
+    sprintf(binstr, "%s%s001%s%s0100011", imm_hi, rs2_bin, rs1_bin, imm_lo);
+    print_and_save(out, binstr);
 }
 
+// Formato B - BNE
 void bne_instr(char *rs1, char *rs2, int imm, FILE *out) {
-    char rs1_bin[6], rs2_bin[6], imm_bin[14];
-    char imm12[2], imm10_5[7], imm4_1[5], imm11[2];
+    char rs1_bin[6], rs2_bin[6], imm_bin[14], imm12[2], imm10_5[7], imm4_1[5], imm11[2], binstr[33];
     reg_to_bin(rs1, rs1_bin);
     reg_to_bin(rs2, rs2_bin);
     imm_to_bin(imm, imm_bin, 13);
@@ -86,30 +116,35 @@ void bne_instr(char *rs1, char *rs2, int imm, FILE *out) {
     strncpy(imm10_5, imm_bin + 1, 6); imm10_5[6] = '\0';
     strncpy(imm4_1, imm_bin + 7, 4); imm4_1[4] = '\0';
     imm11[0] = imm_bin[11]; imm11[1] = '\0';
-    fprintf(out, "%s%s%s001%s%s1100011\n", imm12, imm10_5, rs2_bin, rs1_bin, imm4_1);
+    sprintf(binstr, "%s%s%s001%s%s1100011", imm12, imm10_5, rs2_bin, rs1_bin, imm4_1);
+    print_and_save(out, binstr);
 }
 
 int main(int argc, char *argv[]) {
+    // Verifica se o usuÃ¡rio passou o nome do arquivo .asm
     if (argc < 2) {
         printf("Uso: %s arquivo.asm [-o saida.txt]\n", argv[0]);
         return 1;
     }
 
+    // Abre o arquivo de entrada
     FILE *in = fopen(argv[1], "r");
     if (!in) {
         perror("Erro ao abrir arquivo de entrada");
         return 1;
     }
 
+    // Define a saÃ­da padrÃ£o (terminal), a menos que seja especificado um arquivo
     FILE *out = stdout;
     if (argc == 4 && strcmp(argv[2], "-o") == 0) {
         out = fopen(argv[3], "w");
         if (!out) {
-            perror("Erro ao abrir arquivo de saída");
+            perror("Erro ao abrir arquivo de saÃ­da");
             return 1;
         }
     }
 
+    // LÃª e processa linha por linha do arquivo
     char linha[MAX_LINE];
     while (fgets(linha, MAX_LINE, in)) {
         remove_newline(linha);
@@ -117,6 +152,7 @@ int main(int argc, char *argv[]) {
         char instr[10], op1[10], op2[10], op3[10];
         int lidos = sscanf(linha, "%s %[^,], %[^,], %s", instr, op1, op2, op3);
 
+        // Chama a funÃ§Ã£o correspondente Ã  instruÃ§Ã£o lida
         if (strcmp(instr, "add") == 0 && lidos == 4) {
             add_instr(op1, op2, op3, out);
         } else if (strcmp(instr, "or") == 0 && lidos == 4) {
@@ -132,10 +168,11 @@ int main(int argc, char *argv[]) {
         } else if (strcmp(instr, "bne") == 0 && lidos == 4) {
             bne_instr(op1, op2, atoi(op3), out);
         } else {
-            fprintf(stderr, "Instrução não suportada ou inválida: %s\n", linha);
+            fprintf(stderr, "InstruÃ§Ã£o nÃ£o suportada ou invÃ¡lida: %s\n", linha);
         }
     }
 
+    // Fecha os arquivos
     fclose(in);
     if (out != stdout) fclose(out);
     return 0;
